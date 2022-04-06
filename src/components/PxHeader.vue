5e8378dac29c35b354cbe8df51b3062236d84872
missaelcv <missaelcv0220@gmail.com>
2022-04-06 18:01:29 -0400

    Jodias comillas Simples

--- src/components/PxHeader.vue
+++ src/components/PxHeader.vue
@@ -15,10 +15,10 @@
 </template>
 
 <script>
-import PxIcon from "@/components/PxIcon";
+import PxIcon from '@/components/PxIcon'
 
 export default {
-  name: "PxHeader",
+  name: 'PxHeader',
 
   components: { PxIcon }
 };
