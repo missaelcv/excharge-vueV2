e5a382534e180bcdfc4a55ce947305bf2b3c0c1d
missaelcv <missaelcv0220@gmail.com>
2022-04-21 22:21:58 -0400

    export default router

--- src/router.js
+++ src/router.js
@@ -3,3 +3,14 @@ import Router from 'vue-router'
 import Home from '@/views/Home'
 
 Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+
+  routers: [{
+    path: '/',
+    name: 'home',
+    component: Home
+  }]
+
+})
