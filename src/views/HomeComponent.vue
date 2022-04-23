595d0e943de3f74844b75c17efbac2211f0bce47
missaelcv <missaelcv0220@gmail.com>
2022-04-22 23:25:19 -0400

    HomeComponent

---
+++ src/views/HomeComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <px-assets-table />
+  </div>
+</template>
+
+<script>
+import PxAssetsTable from '@/components/PxAssetsTable'
+
+export default {
+
+  name: 'HomeComponent',
+
+  components: { PxAssetsTable }
+}
+</script>
