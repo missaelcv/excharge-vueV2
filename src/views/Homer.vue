fdd730be2d5051aca50fb42d6d72907881a31bda
missaelcv <missaelcv0220@gmail.com>
2022-04-22 22:30:25 -0400

    ajuste

--- src/views/Homer.vue
+++ src/views/Homer.vue
@@ -1,16 +1,17 @@
 <template>
   <div>
-    <px-assets-table/>
+    <px-assets-table />
   </div>
 </template>
 
 <script>
-
 import PxAssetsTable from '@/components/PxAssetsTable'
 
 export default {
-  name: "Home",
 
-  components: { PxAssetsTable }
+  components: { PxAssetsTable },
+
+  name: 'Home'
+
 }
 </script>
